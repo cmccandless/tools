6b013abe048555511171ee52d0afb44971569ac2
Corey McCandless <corey.mccandless@atlasied.com>
2018-07-02 09:02:37 -0400

    create and run generate.py

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tools
+
+## Table of Contents
+- Web Development
+
+## Web Development
+| Title | Description | Free | Link |
+|:--- |:--- |:---:|:--- |
+| ngrok | One command for an instant, secure URL to your localhost server through any NAT or firewall. | Yes | [Go](https://ngrok.com/) |
+
---
+++ generate.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import markdown_generator as mdg
+import json
+
+
+def get_tool_table_entry(tool):
+    def format_data(k, v):
+        if k == 'Free':
+            return 'Yes' if v else 'No'
+        if k == 'Link':
+            return mdg.link(v, 'Go')
+        return v
+
+    return [
+        format_data(k, tool[k])
+        for k in ['Title', 'Description', 'Free', 'Link']
+    ]
+
+
+with open('tools.json') as f:
+    data = sorted(json.load(f).items())
+
+with open('README.md', 'w') as f:
+    writer = mdg.Writer(f)
+    writer.writeline('# Tools\n')
+
+    writer.writeline('## Table of Contents')
+    for category, _ in data:
+        writer.writeline('- ' + category)
+
+    for category, tools in data:
+        writer.writeline('\n## ' + category)
+        table = mdg.Table()
+        table.add_column('Title')
+        table.add_column('Description')
+        table.add_column('Free', mdg.Alignment.CENTER)
+        table.add_column('Link')
+        for tool in tools:
+            table.append(*get_tool_table_entry(tool))
+        writer.write(table)
