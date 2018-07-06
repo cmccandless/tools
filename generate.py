e4477e76b278ea7ec290f4f490fc52768e504797
Corey McCandless <cmccandless@users.noreply.github.com>
2018-07-06 14:03:47 -0400

    Verify all links are valid (#15)

--- generate.py
+++ generate.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import markdown_generator as mdg
 import json
+
 from jsonschema import validate
 import unittest
+import httplib2
+from http import HTTPStatus
 
 
 with open('tools.json') as f:
@@ -14,6 +17,19 @@ class ToolsTest(unittest.TestCase):
         with open('tools.schema') as f:
             schema = json.load(f)
         validate(data, schema)
+
+    def test_validate_links(self):
+        h = httplib2.Http()
+        for tool in (t for ts in data.values() for t in ts):
+            url = tool['Link']
+            resp = h.request(url, 'HEAD')
+            status = HTTPStatus(int(resp[0]['status']))
+            status_msg = f'{status} {status.name}: {url}'
+            self.assertLess(
+                status,
+                300,
+                status_msg
+            )
 
 
 def get_tool_table_entry(tool):
